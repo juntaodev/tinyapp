a6aa60d54505dca55f9588d4d3be585ac5c10076
JUNTAO LIU <106684251+juntaodev@users.noreply.github.com>
2022-12-01 11:09:22 -0500

    Add registration feature

--- express_server.js
+++ express_server.js
@@ -18,12 +18,30 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+const users = {
+  userRandomID: {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur",
+  },
+  user2RandomID: {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk",
+  },
+};
+
+const getUserByEmail = (userEmail) => {
+  for (const user in users) {
+    if (users[user].email === userEmail) {
+      return true;
+    }
+  }
+  return false;
+};
+
 app.get("/", (req, res) => {
   res.send("Hello!");
-});
-
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
 });
 
 app.get("/urls.json", (req, res) => {
@@ -36,14 +54,15 @@ app.get("/hello", (req, res) => {
 
 app.get("/urls", (req, res)=> {
   const templateVars = {
-    username: req.cookies["username"],
-    urls: urlDatabase
+    user: users[req.cookies["user_id"]],
+    urls: urlDatabase,
+    cookies: req.cookies
   };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  const templateVars = { username: req.cookies["username"] };
+  const templateVars = { user: users[req.cookies["user_id"]], urls: urlDatabase, cookies: req.cookies  };
   res.render("urls_new", templateVars);
 });
 
@@ -56,9 +75,10 @@ app.post("/urls", (req, res) => {
 
 app.get("/urls/:id", (req, res) => {
   const templateVars = {
-    username: req.cookies["username"],
+    user: users[req.cookies["user_id"]],
     shortURL: req.params.id,
-    longURL: urlDatabase[req.params.id]
+    longURL: urlDatabase[req.params.id],
+    cookies: req.cookies
   };
   res.render("urls_show", templateVars);
 });
@@ -81,11 +101,42 @@ app.post("/urls/:id", (req, res) => {
 });
 
 app.post("/login", (req,res) => {
-  res.cookie('username', req.body.username);
+  
   res.redirect("/urls");
 });
 
 app.post("/logout", (req, res) => {
-  res.clearCookie('username');
+  res.clearCookie("user_id");
   res.redirect("/urls");
+});
+
+app.get("/register", (req, res) => {
+  const templateVars = { user: users[req.cookies["user_id"]], cookies: req.cookies };
+  res.render("user_registration", templateVars);
+});
+
+app.post("/register", (req, res) => {
+  const userID = generateRandomString();
+  const userEmail = req.body.email;
+  const password = req.body.password;
+
+  if (userEmail === "" || password === "") {
+    return res.send("400 status code error: Empty field(s), check email and/or password");
+  }
+
+  if (getUserByEmail(userEmail)) {
+    return res.send("400 status code error: Email already exists");
+  }
+
+  users[userID] = {
+    id: userID,
+    email: userEmail,
+    password: password
+  };
+  res.cookie("user_id", userID);
+  res.redirect("/urls");
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
 });
